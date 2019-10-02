cfa0ae0ba61599af2cf1da9eacf50e93e8920e10
Alec Grant <aleclouisgrant@gmail.com>
2019-10-02 15:36:22 -0400

    Add template demo page and basic page loading animations.
    Added a template for each demo page and linked all of the required buttons using JQuery. Also added some simple page animations to the buttons.

--- js/index.js
+++ js/index.js
@@ -5,3 +5,18 @@
 function unhighlightCard(x) {
     x.children[0].children[0].children[0].style.background = "#E8E8E8";
 }
+
+
+function onResize() {
+    console.log("screen.width: " + screen.width);
+    var body = document.getElementsByTagName("body");
+    
+
+    if (screen.width > 1199) {
+        //body[0].style = "overflow-y: hidden;";
+    }
+    else {
+        //body[0].style = "overflow-y: visible;";
+    }
+    
+}
---
+++ js/jq.js
@@ -0,0 +1,47 @@
+﻿/* Fade in on load */
+$(document).ready(function () {
+    $("#card_row").fadeIn(1000);
+});
+
+/* Project Toggles */
+$("#pathfinder-proj-button").on("click", function () {
+    $("#card_row").fadeOut(500);
+    setTimeout(function () {
+        $("#pathfinder-proj").fadeIn(500);
+    }, 500);
+});
+
+$("#shift_report_tool-proj-button").on("click", function () {
+    $("#card_row").fadeOut(500);
+    setTimeout(function () {
+        $("#shift_report_tool-proj").fadeIn(500);
+    }, 500);
+});
+
+$("#melody-proj-button").on("click", function () {
+    $("#card_row").fadeOut(500);
+    setTimeout(function () {
+        $("#melody-proj").fadeIn(500);
+    }, 500);
+});
+
+$("#sms_bingo-proj-button").on("click", function () {
+    $("#card_row").fadeOut(500);
+    setTimeout(function () {
+        $("#sms_bingo-proj").fadeIn(500);
+    }, 500);
+});
+
+
+/* Back Button */
+$(".back-button").on("click", function () {
+    var table_parent = $(this).parentsUntil(".container-fluid").filter("table")[0];
+    fadeToHome(table_parent.id);
+});
+
+function fadeToHome(name) {
+    $("#" + name).fadeOut(500);
+    setTimeout(function () {
+        $("#card_row").fadeIn(500);
+    }, 500);
+}
