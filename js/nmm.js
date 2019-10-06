2de534447b92033f00915e7f2236f0ca6bba8a91
Alec Grant <aleclouisgrant@gmail.com>
2019-10-06 17:07:11 -0400

    Add new move generator application.
    Adds /new-move-maker.html

---
+++ js/nmm.js
@@ -0,0 +1,37 @@
+﻿var moves = [
+    "Sugar push",
+    "Whip",
+    "Right side pass",
+    "Left side pass"
+];
+
+var roles = ["leader's", "follower's"];
+
+var variations = [
+    "fold.",
+    "turn.",
+    "double turn.",
+    "drape.",
+    "drop.",
+    "head duck.",
+    "lean."
+];
+
+
+function generateMove() {
+    var moveText = document.getElementById("move-text");
+    var roleText = document.getElementById("role-text");
+    var variationText = document.getElementById("variation-text");
+
+    moveText.innerText = moves[getRand(moves.length - 1)];
+    roleText.innerText = roles[getRand(roles.length - 1)];
+    variationText.innerText = variations[getRand(variations.length - 1)];
+}
+
+function getRand(length) {
+
+    var min = 0;
+    var max = length;
+
+    return Math.floor(Math.random() * (+max - +min)) + +min;
+}
