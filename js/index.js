63ed1f4f33cc00cec1dbc07d43578dba53dc33c8
Alec Grant <aleclouisgrant@gmail.com>
2019-10-09 11:22:52 -0400

    Add mobile support with header.
    Added a mobile header that displays when screen size gets below 1000px.

--- js/index.js
+++ js/index.js
@@ -6,6 +6,25 @@ function unhighlightCard(x) {
     x.children[0].children[0].children[0].style.background = "#E8E8E8";
 }
 
+function displayHeader() {
+    var d_header = document.getElementById("desktop-header");
+    var m_header = document.getElementById("mobile-header");
+
+    console.log("screen width: " + screen.width);
+
+    if (screen.width < 1000) {
+        d_header.style = "display: none;";
+        m_header.style = "display: block; padding-top: 50px;";
+
+        console.log("setting mobile header");
+    } else {
+        d_header.style = "display: normal;";
+        m_header.style = "display: none;";
+
+        console.log("setting desktop header");
+    }
+}
+
 /*
 var contactAnim = anime({
     targets: '#line-r-2',
